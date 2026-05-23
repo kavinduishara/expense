100a5e6f5bab9e15d8a29ab880d62d38f4000723
kavindu ishara <kavinduishara2923@gmail.com>
2026-05-24 00:40:10 +0530

    conver to war

--- src/main/java/com/example/expense/ExpenseApplication.java
+++ src/main/java/com/example/expense/ExpenseApplication.java
@@ -2,12 +2,18 @@ package com.example.expense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExpenseApplication {
+public class ExpenseApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ExpenseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseApplication.class, args);
 	}
-
 }
